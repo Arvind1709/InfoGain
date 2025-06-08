1f885c81e368ba6a5b2a6a1d64d12ebdf7c16454
Arvind1709 <arvindyadav1709@gmail.com>
2025-06-08 12:18:05 +0530

    Add project files.

---
+++ MyFirstProgramme.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace InfoGain
+{
+    public class MyFirstProgramme
+    {
+        public void FirstProgramme()
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("This is my first C# program.");
+            Console.WriteLine("I am learning how to write C# code.");
+            Console.WriteLine("Let's see how it goes!");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+
+namespace InfoGain
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            MyFirstProgramme programme = new MyFirstProgramme();
+            programme.FirstProgramme();
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
